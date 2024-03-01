9dd3b45c6e147d5038deb19212ccb8a8679699ef
nico-garcia-r <nicolas.garcia@rocketbot.com>
2024-03-01 08:08:20 -0300

    [hidden] add traceback

--- __init__.py
+++ __init__.py
@@ -26,98 +26,101 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
 
 import os
 import sys
+import traceback
 from PIL import Image
 
 SetVar = SetVar # type: ignore
 GetParams = GetParams # type: ignore
 PrintException = PrintException # type: ignore
+try:
+    base_path = tmp_global_obj["basepath"] # type: ignore
+    cur_path = os.path.join(base_path, 'modules', 'OCR_Tesseract', 'libs')
+    sys.path.append(cur_path)
+    cur_path = os.path.join(base_path, 'modules', 'OCR_Tesseract', 'tesseract')
+    sys.path.append(cur_path)
 
-base_path = tmp_global_obj["basepath"] # type: ignore
-cur_path = os.path.join(base_path, 'modules', 'OCR_Tesseract', 'libs')
-sys.path.append(cur_path)
-cur_path = os.path.join(base_path, 'modules', 'OCR_Tesseract', 'tesseract')
-sys.path.append(cur_path)
+    import pyocr # type: ignore
+    import pyocr.tesseract # type: ignore
+    from pyocr.builders import TextBuilder # type: ignore # type: ignore
+    from pyocr.tesseract import CharBoxBuilder # type: ignore
 
-import pyocr # type: ignore
-import pyocr.tesseract # type: ignore
-from pyocr.builders import TextBuilder # type: ignore # type: ignore
-from pyocr.tesseract import CharBoxBuilder # type: ignore
+    # print ocr lib version
+    print("pyocr.tesseract.get_version():", pyocr.tesseract.get_version())
 
-# print ocr lib version
-print("pyocr.tesseract.get_version():", pyocr.tesseract.get_version())
+    """
+        Obtengo el modulo que fueron invocados
+    """
 
-"""
-    Obtengo el modulo que fueron invocados
-"""
+    class Builder(TextBuilder):
+        def __init__(self, **kwargs):
+            psm = "3"
+            if "psm" in kwargs:
+                if kwargs["psm"] is not None: psm = kwargs["psm"]
+                del kwargs["psm"]
+            super().__init__(**kwargs)
+            self.tesseract_configs = ['--oem', '3', '--psm', psm]
+            
+            
+    module = GetParams("module")
 
-class Builder(TextBuilder):
-    def __init__(self, **kwargs):
-        psm = "3"
-        if "psm" in kwargs:
-            if kwargs["psm"] is not None: psm = kwargs["psm"]
-            del kwargs["psm"]
-        super().__init__(**kwargs)
-        self.tesseract_configs = ['--oem', '3', '--psm', psm]
-          
-          
-module = GetParams("module")
+    if module == "gettext":
 
-if module == "gettext":
+        image = GetParams("image")
+        result = GetParams("result")
+        psm = GetParams("psm")
 
-    image = GetParams("image")
-    result = GetParams("result")
-    psm = GetParams("psm")
+        try:
+            pyocr.tesseract.TESSERACT_CMD = os.path.join(base_path,  'modules', 'OCR_Tesseract', 'Tesseract-OCR', 'tesseract.exe')
+            img = Image.open(image)
+            w,h = img.size
+            if w<= 200 or h <= 50:
+                print("Scaling...")
+                factor = min(1, float(1024.0 / w))
+                size = int(factor * w), int(factor * h)
+                img = img.resize(size)
+            # img = improve_image(image)
+            tool = pyocr.get_available_tools()[0]
+            lang = tool.get_available_languages()[0]
 
-    try:
-        pyocr.tesseract.TESSERACT_CMD = os.path.join(base_path,  'modules', 'OCR_Tesseract', 'Tesseract-OCR', 'tesseract.exe')
-        img = Image.open(image)
-        w,h = img.size
-        if w<= 200 or h <= 50:
-            print("Scaling...")
-            factor = min(1, float(1024.0 / w))
-            size = int(factor * w), int(factor * h)
-            img = img.resize(size)
-        # img = improve_image(image)
-        tool = pyocr.get_available_tools()[0]
-        lang = tool.get_available_languages()[0]
+            text = tool.image_to_string(img,lang=lang, builder=Builder(psm=psm))
 
-        text = tool.image_to_string(img,lang=lang, builder=Builder(psm=psm))
+            if result:
+                SetVar(result, text)
 
-        if result:
-            SetVar(result, text)
+        except Exception as e:
+            PrintException()
+            raise e
+        
+    if module == "get_charboxes":
+        image = GetParams("image")
+        result = GetParams("result")
 
-    except Exception as e:
-        PrintException()
-        raise e
-    
-if module == "get_charboxes":
-    image = GetParams("image")
-    result = GetParams("result")
+        try:
+            img = Image.open(image)
+            w,h = img.size
+            if w<= 200 or h <= 50:
+                print("Scaling...")
+                factor = min(1, float(1024.0 / w))
+                size = int(factor * w), int(factor * h)
+                img = img.resize(size)
+            # img = improve_image(image)
+            tool = pyocr.get_available_tools()[0]
+            lang = tool.get_available_languages()[0]
 
-    try:
-        img = Image.open(image)
-        w,h = img.size
-        if w<= 200 or h <= 50:
-            print("Scaling...")
-            factor = min(1, float(1024.0 / w))
-            size = int(factor * w), int(factor * h)
-            img = img.resize(size)
-        # img = improve_image(image)
-        tool = pyocr.get_available_tools()[0]
-        lang = tool.get_available_languages()[0]
+            boxes = []
+            # get each character box
+            for box in tool.image_to_string(img, lang=lang, builder=CharBoxBuilder()):
+                boxes.append([box.content, box.position])
 
-        boxes = []
-        # get each character box
-        for box in tool.image_to_string(img, lang=lang, builder=CharBoxBuilder()):
-            boxes.append([box.content, box.position])
+            if result:
+                SetVar(result, boxes)
 
-        if result:
-            SetVar(result, boxes)
+        except Exception as e:
+            PrintException()
+            raise e
 
-    except Exception as e:
-        PrintException()
-        raise e
-
-    
+except Exception as e:
+    traceback.print_exc()
+    raise e
 
 
